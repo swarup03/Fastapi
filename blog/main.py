d4b49722605a6dcd19a27f45bc9924ed05425119
Swarup shah <swarupshah03@hotmail.com>
2024-09-17 00:19:19 +0530

    push and get

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,6 @@
-from typing import Union
-
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -9,7 +9,35 @@ app = FastAPI()
 def read_root():
     return {"Hello": "Swarup Shah"}
 
+@app.get("/items/users")
+def read_item_user():
+    return {"item_id": "item"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/users/{user_id}")
+def read_parameter(user_id: int, limit:int = 10, sort: Optional[bool] = False):
+    if sort:
+        return {
+            "data": f"This is sorted blogs of user {user_id}",
+            "blog langth":limit
+        }
+    else:
+        return {
+                "data": f"This is unsorted blogs of user {user_id}",
+                "blog langth":limit
+            }
+class User(BaseModel):
+    name: str
+    age: int
+    sex: Optional[str] = 'male'
+
+
+@app.post("/user")
+def create_user(user: User):
+    return {
+        'message':'create user',
+        'data':user
+    }
